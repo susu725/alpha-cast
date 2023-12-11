b78f91bbe3cd20e179adc9e6ce9af80ddb1e5424
susu725 <zxcv1010017@gmail.com>
2023-12-11 21:50:37 +0800

    feat: setting router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthContext } from './contexts/AuthContext'
+import MainPage from './pages/MainPage'
+import './App.scss'
+import LoginPage from './pages/LoginPage'
 
 function App() {
+  const [token, setToken] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div basename='/alpha-cast'>
+      <AuthContext.Provider value={{ token, setToken }}>
+        <BrowserRouter basename='/alpha-cast'>
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/home/:id' element={<MainPage />} />
+            <Route path='/' element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
